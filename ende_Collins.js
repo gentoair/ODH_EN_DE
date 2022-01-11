021c5288872ae04b5d375d4ee77434e683f3e343
gentoair <73964422+gentoair@users.noreply.github.com>
2022-01-11 20:05:11 +0100

    Add files via upload

---
+++ ende_Collins.js
@@ -0,0 +1,95 @@
+﻿/* global api */
+class ende_Collins {
+    constructor(options) {
+        this.options = options;
+        this.maxexample = 2;
+        this.word = '';
+    }
+
+    async displayName() {
+        let locale = await api.locale();
+        if (locale.indexOf('CN') != -1) return 'Collins EN->DE Dictionary';
+        if (locale.indexOf('TW') != -1) return 'Collins EN->DE Dictionary';
+        return 'Collins EN->DE Dictionary';
+    }
+
+    setOptions(options) {
+        this.options = options;
+        this.maxexample = options.maxexample;
+    }
+
+    async findTerm(word) {
+        this.word = word;
+        return await this.findCollins(word);
+    }
+
+    async findCollins(word) {
+        if (!word) return null;
+
+        let base = 'https://www.collinsdictionary.com/dictionary/english-german/';
+        let url = base + encodeURIComponent(word);
+        let doc = '';
+        try {
+            let data = await api.fetch(url);
+            let parser = new DOMParser();
+            doc = parser.parseFromString(data, 'text/html');
+        } catch (err) {
+            return null;
+        }
+
+        let content = doc.querySelector('.content') || '';
+        let pron = doc.querySelector('.form.pron.type-') || '';
+        if (!pron) return null;
+        if (!content) return null;
+        let css = this.renderCSS();
+        return css + pron.innerHTML + content.innerHTML;
+    }
+
+    renderCSS() {
+        let css = `
+            <style>
+                .copyright{
+                    display:none;
+                }
+                .orth {
+                    font-size: 100%;
+                    font-weight: bold;
+                }
+                .cit.type-translation.quote {
+                    font-style: normal;
+                    font-weight: bold;
+                    color: #262323;
+                }
+                .quote {
+                    font-style: normal;
+                    color: #1683be;
+                }
+                .colloc {
+                    font-style: italic;
+                    font-weight: normal;
+                }
+                .sense {
+                    /*border: 1px solid;*/
+                    /*border-color: #e5e6e9 #dfe0e4 #d0d1d5;*/
+                    border-radius: 3px;
+                    padding: 5px;
+                    margin: 5px 0;
+                    background-color: #f6f6f6;
+                }
+                .sense .re {
+                    font-size: 100%;
+                    margin-left: 0;
+                }
+                a {
+                    color: #000;
+                    /*text-decoration: none;*/
+                }
+                * {
+                    word-wrap: break-word;
+                    box-sizing: border-box;
+                }
+            </style>`;
+
+        return css;
+    }
+}
